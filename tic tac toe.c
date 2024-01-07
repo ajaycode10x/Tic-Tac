84a9349a6ba86fea3701778f313f1e5919405ec4
Ajay Kumar <151925422+ajaycode10x@users.noreply.github.com>
2024-01-07 08:17:48 +0530

    Add files via upload

---
+++ tic tac toe.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n");
+    printf(" == Tic Tac Toe == \n");
+    printf("     |      |    \n");
+    printf("     |      |    \n");
+    printf("  1  |  2   |  3 \n");
+    printf("     |      |    \n");
+    printf("     |      |    \n");
+    printf("  4  |  5   |  6 \n");
+    printf("     |      |    \n");
+    printf("     |      |    \n");
+    printf("  7  |  8   |  9 \n");
+   return 0;
+}
